2cf308f7285c0d5e3628bdf33f8ff90cd8fa0aea
thaianealm <cont.thaiane@gmail.com>
2022-08-09 19:02:36 -0300

    banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="main-banner">
+        <h1>Monte <br/>Seu <br/>Hambúrguer</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Banner',
+    }
+</script>
+
+<style scoped>
+    .main-banner{
+        display: flex;
+        justify-content: flex-start;
+        height: 80vh;
+        background-image: url('/public/img/burger-banner.jpg');
+        background-position: 0 -380px;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    .main-banner h1{
+        height: 70px;
+        width: 50%;
+        color: #fff;
+        font-size: 60pt;
+        text-align: left;
+        margin-left: 5%;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte seu Hambúrguer</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue';
 // @ is an alias to /src
 
 
 export default {
   name: 'HomeView',
   components: {
-    
-  }
+    Banner
+}
 }
 </script>
