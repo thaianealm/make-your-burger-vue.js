3d18705adeb415e121615af28d34185a5b9bb471
thaianealm <cont.thaiane@gmail.com>
2022-08-23 09:40:17 -0300

    delete system/database requests

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -28,7 +28,7 @@
                         <option value="">Selecione</option>
                         <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -58,10 +58,18 @@ export default{
         },
         async getStatus() {
 
-            const req = await fetch('http://localhost:3000/status')
-            const data = await req.json()
-            this.status = data
+            const req = await fetch('http://localhost:3000/status');
+            const data = await req.json();
+            this.status = data;
       },
+        async deleteBurger(id) {
+            const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+              method: 'DELETE'
+            });
+            const res = await req.json();
+
+            this.getRequests();
+      }
     },
     mounted(){
         this.getRequests();
